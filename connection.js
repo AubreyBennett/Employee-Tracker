9742c84dfd92aaf4c3751f699eb61eaadb97dbb2
Aubrey Bennett <27aubreyb@gmail.com>
2021-03-11 14:38:29 -0500

    initial search inquirer prompt created

--- connection.js
+++ connection.js
@@ -1,4 +1,5 @@
 const mysql = require("mysql");
+const inquirer = require("inquirer");
 
 const connection = mysql.createConnection({
     host: "localhost",
@@ -13,3 +14,45 @@ connection.connect(err => {
     console.log(`We connected! Connected as thread id ${connection.threadId}`);
     connection.end();
 });
+
+function startSearch() {
+    inquirer
+        .prompt({
+            name: "action",
+            type: "list",
+            message: "What would you like to do?",
+            choices: [
+                "View all employees",
+                "Add employee",
+                "Update employee role",
+                "View all departments",
+                "Add department",
+                "View all roles",
+                "Add role"
+            ]
+        }).then(function(answer) {
+            switch (answer.action) {
+                case "View all employees":
+                    employeeSearch();
+                    break;
+                case "Add employee":
+                    employeeAdd();
+                    break;
+                case "Update employee role":
+                    employeeRole();
+                    break;
+                case "View all departments":
+                    departmentSearch();
+                    break;
+                case "Add department":
+                    departmentAdd();
+                    break;
+                case "View all roles":
+                    roleSearch();
+                    break;
+                case "Add role":
+                    roleAdd();
+                    break;
+            }
+        })
+};
